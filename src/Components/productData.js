c3fe3eeedfb097f67f6d76f090abda90d880a59e
Brentyn-Delport <brentyn.delport@gmail.com>
2024-01-05 19:40:39 +0200

    Added the correct meme images for the site

--- src/Components/productData.js
+++ src/Components/productData.js
@@ -4,25 +4,15 @@ import { Form } from "react-router-dom";
 
 import memeFront1 from '../Images/1memeFront.jpeg';
 import memeBack1 from '../Images/1memeBack.jpeg';
-import memeFront2 from '../Images/2memeFront.jpeg';
 import memeBack2 from '../Images/2memeBack.jpeg';
-import memeFront3 from '../Images/3memeFront.jpeg';
 import memeBack3 from '../Images/3memeBack.jpeg';
-import memeFront4 from '../Images/4memeFront.jpeg';
 import memeBack4 from '../Images/4memeBack.jpeg';
-import memeFront5 from '../Images/5memeFront.jpeg';
 import memeBack5 from '../Images/5memeBack.jpeg';
-import memeFront6 from '../Images/6memeFront.jpeg';
 import memeBack6 from '../Images/6memeBack.jpeg';
-import memeFront7 from '../Images/7memeFront.jpeg';
 import memeBack7 from '../Images/7memeBack.jpeg';
-import memeFront8 from '../Images/8memeFront.jpeg';
 import memeBack8 from '../Images/8memeBack.jpeg';
-import memeFront9 from '../Images/9memeFront.jpeg';
 import memeBack9 from '../Images/9memeBack.jpeg';
-import memeFront10 from '../Images/10memeFront.jpeg';
 import memeBack10 from '../Images/10memeBack.jpeg';
-import memeFront11 from '../Images/11memeFront.jpeg';
 import memeBack11 from '../Images/11memeBack.jpeg';
 
 
